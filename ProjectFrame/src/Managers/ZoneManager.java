78aa79c931f7d4b5065cf92e66ec4530dea724f8
Iker Ruiz Arnauda <iker_arnauda@hotmail.com>
2013-04-24 04:44:57 -0500

    Refactored GetZonesInRange Method to avoid duplicate logic.

--- ProjectFrame/src/Managers/ZoneManager.java
+++ ProjectFrame/src/Managers/ZoneManager.java
@@ -44,7 +44,7 @@ public class ZoneManager  {
 	private static Bag<Entity> inRangeEntities = new Bag<Entity>(); //Stores the ones that are in range. < We return this from our method.
 	private static WorldPositionComponent entityCallerPosition; //X,Y for the entity asking for entities within range.
 	private static WorldPositionComponent foundEntityPosition; //The position of a found entity
-	
+	private static String _typeName;
 	/**
 	 * @param e
 	 * @param com
@@ -52,87 +52,49 @@ public class ZoneManager  {
 	 * @return 
 	 */
 	public static ImmutableBag<Entity> GetZonesInRange(Entity e, ZoneTypes Ttype, int Range) {
-		inRangeEntities.clear(); //Clear our inRangeEntities for a clean start!
+		
+		//Assign ZoneType name.
 		switch(Ttype){
-			case Res:	
-				entityCallerPosition = e.getComponent(WorldPositionComponent.class);
-				Logger.Log("Entity Caller Coords:" + "X" + (entityCallerPosition.GetCoords())[0] + "Y" + (entityCallerPosition.GetCoords())[1]);
-				
-				//Grab the entities within range of the caller entity.
-				foundEntities = e.getWorld().getManager(GroupManager.class).getEntities("Residential");				
-				for(int i = 0; i < foundEntities.size(); i++)
-				{
-					//Get the WorldPosition of the entity
-					foundEntityPosition = foundEntities.get(i).getComponent(WorldPositionComponent.class);
-					if(
-							//Evaluate if its within the given range
-							entityCallerPosition.GetCoords()[0]  + Range > (foundEntityPosition.GetCoords()[0]) //X Axis
-							&& entityCallerPosition.GetCoords()[0] - Range < foundEntityPosition.GetCoords()[0] //X Axis
-							&& entityCallerPosition.GetCoords()[1]  + Range > (foundEntityPosition.GetCoords()[1]) //Y Axis
-							&& entityCallerPosition.GetCoords()[1] - Range < foundEntityPosition.GetCoords()[1] //Y Axis
-					)
-						
-					{	//If it is in the right range we add it to the <Entity> Bag that we will return.
-						inRangeEntities.add(foundEntities.get(i));
-					}
-				}
-		
-				Logger.Log("In range entities: " + Integer.toString(inRangeEntities.size())); //Activate this line to see the magic working.
-				return inRangeEntities;
+			case Res:
+				_typeName = "Residential";				
+				break;
+			
 			case Com:
-				entityCallerPosition = e.getComponent(WorldPositionComponent.class);
-				Logger.Log("Entity Caller Coords:" + "X" + (entityCallerPosition.GetCoords())[0] + "Y" + (entityCallerPosition.GetCoords())[1]);
+				_typeName = "Commercial";				
+				break;
 				
-				//Grab the entities within range of the caller entity.
-				foundEntities = e.getWorld().getManager(GroupManager.class).getEntities("Commercial");				
-				for(int i = 0; i < foundEntities.size(); i++)
-				{
-					//Get the WorldPosition of the entity
-					foundEntityPosition = foundEntities.get(i).getComponent(WorldPositionComponent.class);
-					if(
-							//Evaluate if its within the given range
-							entityCallerPosition.GetCoords()[0]  + Range > (foundEntityPosition.GetCoords()[0]) //X Axis
-							&& entityCallerPosition.GetCoords()[0] - Range < foundEntityPosition.GetCoords()[0] //X Axis
-							&& entityCallerPosition.GetCoords()[1]  + Range > (foundEntityPosition.GetCoords()[1]) //Y Axis
-							&& entityCallerPosition.GetCoords()[1] - Range < foundEntityPosition.GetCoords()[1] //Y Axis
-					)
-						
-					{	//If it is in the right range we add it to the <Entity> Bag that we will return.
-						inRangeEntities.add(foundEntities.get(i));
-					}
-				}
-		
-				//Logger.Log("In range entities: " + Integer.toString(inRangeEntities.size())); //Activate this line to see the magic live.
-				return inRangeEntities;
 			case Ind:
-				entityCallerPosition = e.getComponent(WorldPositionComponent.class);
-				Logger.Log("Entity Caller Coords:" + "X" + (entityCallerPosition.GetCoords())[0] + "Y" + (entityCallerPosition.GetCoords())[1]);
-				
-				//Grab the entities within range of the caller entity.
-				foundEntities = e.getWorld().getManager(GroupManager.class).getEntities("Industrial");				
-				for(int i = 0; i < foundEntities.size(); i++)
-				{
-					//Get the WorldPosition of the entity
-					foundEntityPosition = foundEntities.get(i).getComponent(WorldPositionComponent.class);
-					if(
-							//Evaluate if its within the given range
-							entityCallerPosition.GetCoords()[0]  + Range > (foundEntityPosition.GetCoords()[0]) //X Axis
-							&& entityCallerPosition.GetCoords()[0] - Range < foundEntityPosition.GetCoords()[0] //X Axis
-							&& entityCallerPosition.GetCoords()[1]  + Range > (foundEntityPosition.GetCoords()[1]) //Y Axis
-							&& entityCallerPosition.GetCoords()[1] - Range < foundEntityPosition.GetCoords()[1] //Y Axis
-					)
-						
-					{	//If it is in the right range we add it to the <Entity> Bag that we will return.
-						inRangeEntities.add(foundEntities.get(i));
-					}
-				}
-		
-				//Logger.Log("In range entities: " + Integer.toString(inRangeEntities.size())); //Activate this line to see the magic live.
-				return inRangeEntities;
+				_typeName = "Industrial";				
+				break;
 			default:
 				return null;
-		}		
-		//return new Entity[0];
+		}	
+		
+		inRangeEntities.clear(); //Clear our inRangeEntities for a clean start!
+		entityCallerPosition = e.getComponent(WorldPositionComponent.class);
+		Logger.Log("Entity Caller Coords:" + "X" + (entityCallerPosition.GetCoords())[0] + "Y" + (entityCallerPosition.GetCoords())[1]);
+		
+		//Grab the entities within range of the caller entity.
+		foundEntities = e.getWorld().getManager(GroupManager.class).getEntities(_typeName);				
+		for(int i = 0; i < foundEntities.size(); i++)
+		{
+			//Get the WorldPosition of the entity
+			foundEntityPosition = foundEntities.get(i).getComponent(WorldPositionComponent.class);
+			if(
+					//Evaluate if its within the given range
+					entityCallerPosition.GetCoords()[0]  + Range > (foundEntityPosition.GetCoords()[0]) //X Axis
+					&& entityCallerPosition.GetCoords()[0] - Range < foundEntityPosition.GetCoords()[0] //X Axis
+					&& entityCallerPosition.GetCoords()[1]  + Range > (foundEntityPosition.GetCoords()[1]) //Y Axis
+					&& entityCallerPosition.GetCoords()[1] - Range < foundEntityPosition.GetCoords()[1] //Y Axis
+			)
+				
+			{	//If it is in the right range we add it to the <Entity> Bag that we will return.
+				inRangeEntities.add(foundEntities.get(i));
+			}
+		}
+
+		Logger.Log("In range entities: " + Integer.toString(inRangeEntities.size())); //Activate this line to see the magic working.
+		return inRangeEntities;
 	}	
 }
 
